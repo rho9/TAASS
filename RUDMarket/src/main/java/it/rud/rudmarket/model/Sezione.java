877bbd604c811b99aea051c34aa62a7055fb9dcc
rho9 <48881429+rho9@users.noreply.github.com>
2019-11-25 12:08:13 +0100

    join table between prodotto and Sezione

--- RUDMarket/src/main/java/it/rud/rudmarket/model/Sezione.java
+++ RUDMarket/src/main/java/it/rud/rudmarket/model/Sezione.java
@@ -2,6 +2,9 @@ package it.rud.rudmarket.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Sezione {
@@ -9,12 +12,18 @@ public class Sezione {
 	@Id
 	private String nome;
 
+	@OneToMany(targetEntity = Prodotto.class)
+	@JoinColumn(name = "sezione_name")
+	private List<Prodotto> prodottoList;
+
+
 	public Sezione() {
-		this(null);
+		this(null, null);
 	}
 
-	public Sezione(String nome) {
+	public Sezione(String nome, List<Prodotto> prodottoList) {
 		this.nome = nome;
+		this.prodottoList = prodottoList;
 	}
 
 	public String getNome() {
@@ -23,5 +32,13 @@ public class Sezione {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List getProdottoList() {
+		return prodottoList;
+	}
+
+	public void setProdottoList(List<Prodotto> prodottoList) {
+		this.prodottoList = prodottoList;
 	}
 }
