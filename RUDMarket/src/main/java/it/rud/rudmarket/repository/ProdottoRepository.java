66792e1f943aa2cd74e05342c578e117719c963c
Davide G <giosa.davide@gmail.com>
2019-11-27 12:54:46 +0100

    start social_login

--- RUDMarket/src/main/java/it/rud/rudmarket/repository/ProdottoRepository.java
+++
@@ -1,8 +0,0 @@
-package it.rud.rudmarket.repository;
-
-import it.rud.rudmarket.model.Prodotto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdottoRepository extends JpaRepository<Prodotto, String> {
-
-}
--- RUDMarket/src/main/java/it/rud/rudmarket/repository/SezioneRepository.java
+++
@@ -1,8 +0,0 @@
-package it.rud.rudmarket.repository;
-
-import it.rud.rudmarket.model.Sezione;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SezioneRepository extends JpaRepository<Sezione, String> {
-
-}
