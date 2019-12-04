f8bf94fed507d860748d84a6932f677e6b21368c
Davide G <giosa.davide@gmail.com>
2019-12-04 11:22:27 +0100

    to add sezione

---
+++ RUDMarket/react-social/src/catalogo/Sezione.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Prodotto.css';
+
+
+class Prodotto extends Component {
+    render() {
+        return(
+            <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                <h1 className="display-4">Pricing</h1>
+                <p className="lead">Quickly build an effective pricing table for your potential customers with this
+                    Bootstrap example. It's built with default Bootstrap components and utilities with little
+                    customization.</p>
+            </div>
+        );
+    }
+}
+
+export default Prodotto;
