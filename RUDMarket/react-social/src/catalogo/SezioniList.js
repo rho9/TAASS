e3fbeb6313542ed161d6669723fb54e690f862fe
Davide G <giosa.davide@gmail.com>
2019-12-04 11:37:59 +0100

    added sezione name

--- RUDMarket/react-social/src/catalogo/SezioniList.js
+++ RUDMarket/react-social/src/catalogo/SezioniList.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import {NavLink} from "react-router-dom";
-import Sezione from "./Sezione";
+import {Link} from "react-router-dom";
 
 const SezioniList = ({ sezioni }) => {
     return(
@@ -12,14 +11,15 @@ const SezioniList = ({ sezioni }) => {
                             <p className="card-text">{sezione.nome}</p>
                             <div className="d-flex justify-content-between align-items-center">
                                 <div className="btn-group">
-                                    <NavLink className="btn btn-warning" to={
+                                    <Link className="btn btn-warning" to={
                                         {
                                             pathname: "/sezione",
-                                            sezioneProps: {
-                                                nomeSezione: sezione.nome
-                                            }
+                                            nomeSezione: sezione.nome,
+                                            prodottoList: sezione.prodottoList
                                         }
-                                    }>Visualizza</NavLink>
+                                    }>
+
+                                    Visualizza</Link>
                                 </div>
                             </div>
                         </div>
