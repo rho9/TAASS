82b2130af3b29ce044675d536967de955a387ba5
rho9 <48881429+rho9@users.noreply.github.com>
2019-12-14 16:15:54 +0100

    every user has his own carrello

--- RUDMarket/spring-social/src/main/java/com/rud/rudmarket/controller/CarrelloController.java
+++ RUDMarket/spring-social/src/main/java/com/rud/rudmarket/controller/CarrelloController.java
@@ -51,14 +51,17 @@ public class CarrelloController {
   	}
     
 	@RequestMapping("/getProdottiInCarrello")
-	public List<Prodotto> getProdottiInCarrello() {
+	public List<Prodotto> getProdottiInCarrello(@CurrentUser UserPrincipal userPrincipal) {
 		List<Carrello> carrelloList = new ArrayList<>();
 		List<Prodotto> prodottoList = new ArrayList<>();
 		Prodotto prodotto = new Prodotto();
 		carrelloList = carrelloRepository.findAll();
+		Long userId = userPrincipal.getId();
 		for(Carrello c : carrelloList){
-			prodotto = c.getProdotto();
-			prodottoList.add(prodotto);
+			if(userId == c.getUser().getId()) {
+				prodotto = c.getProdotto();
+				prodottoList.add(prodotto);
+			}
 		}
 		return prodottoList;
 	}
