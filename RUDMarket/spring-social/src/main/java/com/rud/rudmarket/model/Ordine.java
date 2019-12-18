2804710b41c1db73b142e3da5e772c721def919a
Davide G <giosa.davide@gmail.com>
2019-12-18 17:07:59 +0100

    edited entity ordine

--- RUDMarket/spring-social/src/main/java/com/rud/rudmarket/model/Ordine.java
+++ RUDMarket/spring-social/src/main/java/com/rud/rudmarket/model/Ordine.java
@@ -16,6 +16,56 @@ public class Ordine {
 	@JoinColumn(name = "prodotto_id")
 	Prodotto prodotto;
 
+	private String nome, cognome, indirizzo, supermercatoId;
+
+	public String getSupermercatoId() {
+		return supermercatoId;
+	}
+
+	public void setSupermercatoId(String supermercatoId) {
+		this.supermercatoId = supermercatoId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Prodotto getProdotto() {
+		return prodotto;
+	}
+
+	public void setProdotto(Prodotto prodotto) {
+		this.prodotto = prodotto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
 	public String getId() {
 		return id;
 	}
