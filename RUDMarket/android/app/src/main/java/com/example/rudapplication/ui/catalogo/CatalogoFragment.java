66995e195d6717e499192804f346d2a2165741e1
Davide G <giosa.davide@gmail.com>
2019-12-22 21:55:19 +0100

    added cardview in catalogo fragment

--- RUDMarket/android/app/src/main/java/com/example/rudapplication/ui/catalogo/CatalogoFragment.java
+++ RUDMarket/android/app/src/main/java/com/example/rudapplication/ui/catalogo/CatalogoFragment.java
@@ -1,11 +1,13 @@
 package com.example.rudapplication.ui.catalogo;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +19,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,6 +33,7 @@ import com.example.rudapplication.MainActivity;
 import com.example.rudapplication.R;
 import com.example.rudapplication.api.SezioniAPI;
 import com.example.rudapplication.model.Sezione;
+import com.example.rudapplication.utils.MyRecyclerViewAdapter;
 import com.example.rudapplication.utils.RetrofitUtil;
 
 import java.util.ArrayList;
@@ -35,7 +41,9 @@ import java.util.ArrayList;
 public class CatalogoFragment extends Fragment {
 
     private CatalogoViewModel catalogoViewModel;
-    private ListView catalogoList;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     private RetrofitUtil retrofitUtil;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -45,7 +53,12 @@ public class CatalogoFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_catalogo, container, false);
 
         retrofitUtil = RetrofitUtil.getInstance();
-        catalogoList = (ListView)root.findViewById(R.id.catalogo_list_id);
+        //catalogoList = (ListView)root.findViewById(R.id.catalogo_list_id);
+        mRecyclerView = (RecyclerView) root.findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
         ArrayList<String> arrayList = new ArrayList<>();
         SezioniAPI sezioniAPI = retrofitUtil.getRetrofit().create(SezioniAPI.class);
         Call<List<Sezione>> call2 = sezioniAPI.getSezioni();
@@ -57,8 +70,16 @@ public class CatalogoFragment extends Fragment {
                 for(Sezione sezione: sezioneList){
                     arrayList.add(sezione.getNome());
                 }
-                ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, arrayList);
-                catalogoList.setAdapter(arrayAdapter);
+                //listAdapter =
+                //catalogoList.setAdapter(arrayAdapter);
+                mAdapter = new MyRecyclerViewAdapter(arrayList);
+                mRecyclerView.setAdapter(mAdapter);
+
+                // Code to Add an item with default animation
+                //((MyRecyclerViewAdapter) mAdapter).addItem(obj, index);
+
+                // Code to remove an item with default animation
+                //((MyRecyclerViewAdapter) mAdapter).deleteItem(index);
             }
 
             @Override
@@ -68,5 +89,56 @@ public class CatalogoFragment extends Fragment {
         });
 
         return root;
+    }
+
+    public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>
+    {
+        private ArrayList<String> dataList;
+
+        public ListAdapter(ArrayList<String> data)
+        {
+            this.dataList = data;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder
+        {
+            TextView text;
+
+            public ViewHolder(View itemView)
+            {
+                super(itemView);
+                this.text = (TextView) itemView.findViewById(R.id.text);
+            }
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view, parent, false);
+
+            ViewHolder viewHolder = new ViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ListAdapter.ViewHolder holder, final int position)
+        {
+            holder.text.setText(dataList.get(position));
+
+            holder.itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    Toast.makeText(getActivity(), "Item " + position + " is clicked.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return dataList.size();
+        }
     }
 }
---
+++ RUDMarket/android/app/src/main/java/com/example/rudapplication/utils/MyRecyclerViewAdapter.java
@@ -0,0 +1,76 @@
+package com.example.rudapplication.utils;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.rudapplication.R;
+
+import java.util.ArrayList;
+
+public class MyRecyclerViewAdapter extends RecyclerView
+        .Adapter<MyRecyclerViewAdapter
+        .DataObjectHolder> {
+
+    private ArrayList<String> mDataset;
+    private static MyClickListener myClickListener;
+    public static class DataObjectHolder extends RecyclerView.ViewHolder
+            implements View
+            .OnClickListener {
+
+        TextView label;
+        TextView dateTime;
+
+        public DataObjectHolder(View itemView) {
+            super(itemView);
+            label = (TextView) itemView.findViewById(R.id.text);
+            itemView.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View v) {
+            myClickListener.onItemClick(getAdapterPosition(), v);
+        }
+    }
+    public void setOnItemClickListener(MyClickListener myClickListener) {
+        this.myClickListener = myClickListener;
+    }
+    public MyRecyclerViewAdapter(ArrayList<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public DataObjectHolder onCreateViewHolder(ViewGroup parent,
+                                               int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view, parent, false);
+        DataObjectHolder dataObjectHolder = new DataObjectHolder(view);
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DataObjectHolder holder, int position) {
+        holder.label.setText(mDataset.get(position));
+    }
+
+    public void addItem(String dataObj, int index) {
+        mDataset.add(index, dataObj);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public interface MyClickListener {
+        public void onItemClick(int position, View v);
+    }
+}
