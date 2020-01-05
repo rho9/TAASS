adcb9b93b3c0f837cc485fa20730a88b36c1fd6f
Davide G <giosa.davide@gmail.com>
2020-01-05 17:14:35 +0100

    removed some useless code

--- RUDMarket/spring-social/src/main/java/com/rud/rudmarket/repository/ProdottoImageRepository.java
+++ RUDMarket/spring-social/src/main/java/com/rud/rudmarket/repository/ProdottoImageRepository.java
@@ -1,7 +1,5 @@
 package com.rud.rudmarket.repository;
 
-
-import com.rud.rudmarket.model.Prodotto;
 import com.rud.rudmarket.model.ProdottoImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
