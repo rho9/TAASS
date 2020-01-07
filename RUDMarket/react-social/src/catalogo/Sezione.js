25a25710863a9ac101877d13a9f486e2978b1013
Davide G <giosa.davide@gmail.com>
2020-01-07 12:18:06 +0100

    removed useless code

--- RUDMarket/react-social/src/catalogo/Sezione.js
+++ RUDMarket/react-social/src/catalogo/Sezione.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
 import './Sezione.css';
 import ProdottoList from "./ProdottoList";
-import {API_BASE_URL} from "../constants";
 
 class Sezione extends Component {
     constructor(props) {
         super(props);
-        //alert(this.props.location.prodottoList);
         this.state = {
             prodotti: this.props.location.prodottoList
         }
-
-        //this.componentDidMount.bind(this)
-        this.setState({prodotti : this.props.location.prodottoList})
     }
-
-    /*componentDidMount() {
-        fetch(API_BASE_URL + '/sezione/getProdottiByIdSezione', {
-            method: 'POST',
-            body: this.props.location.idSezione
-        })
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ prodotti: data })
-            })
-    }*/
 
     render() {
         return(
