5a00e40933fd3bf03b2e4d5e6d1fef858cd213a6
Davide G <giosa.davide@gmail.com>
2020-01-09 11:21:03 +0100

    fixed popup

--- RUDMarket/react-social/src/carrello/PopupContent.js
+++ RUDMarket/react-social/src/carrello/PopupContent.js
@@ -1,20 +1,64 @@
 import React, {Component} from "react";
 import './Carrello.css'
 
+class PopupContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ricette: [],
+        };
 
-export default ({ close, parametro }) => (
+        fetch('https://spring-efp.herokuapp.com/recipeFromIngredient/' + this.props.titoloRicetta)
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ricette: data})
+            })
+    }
+
+    render() {
+        return(
+            <div>
+                <a onClick={this.props.close}>
+                    &times;
+                </a>
+                <h2 align="center">Ricette con {this.props.titoloRicetta}</h2>
+                <section className="jumbotron text-center">
+                    <div className="table-responsive">
+                        <div className="table-responsive">
+                            <table className="table table-striped table-sm">
+                                <tbody>
+                                {
+                                    this.state.ricette.map((ricetta) => (
+                                        <tr>
+                                            <th>{ricetta.title}</th>
+                                        </tr>
+                                    ))
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+
+export default PopupContent;
+
+/*export default ({ close, titoloRicetta, ricette }) => (
     <div>
         <a onClick={close}>
             &times;
         </a>
-        <h2 align="center">Ricette con {parametro} </h2>
+        <h2 align="center">Ricette con {titoloRicetta} </h2>
         <section className="jumbotron text-center">
             <div className="table-responsive">
                 <div className="table-responsive">
                     <table className="table table-striped table-sm">
                         <tbody>
                             <tr>
-                                <th>Ciao</th>
+                                <th>{ricette}</th>
                             </tr>
                         </tbody>
                     </table>
@@ -22,4 +66,4 @@ export default ({ close, parametro }) => (
             </div>
         </section>
     </div>
-);
+);*/
--- RUDMarket/react-social/src/carrello/ProdottiInCarrello.js
+++ RUDMarket/react-social/src/carrello/ProdottiInCarrello.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './Carrello.css'
 import PopupContent from "./PopupContent";
 import Popup from "reactjs-popup";
 
-const ProdottiInCarrello = ({ prodottiInCarrello }) => {
+class ProdottiInCarrello extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <tbody>
+            {this.props.prodottiInCarrello.map((prodottoInCarrello) => (
+                <tr>
+                    <th>{prodottoInCarrello.quantita}</th>
+                    <th>{prodottoInCarrello.prodotto.nome}</th>
+                    <th>{prodottoInCarrello.prodotto.marca}</th>
+                    {
+                        prodottoInCarrello.percSconto == 0 ? (
+                            <th>{prodottoInCarrello.prodotto.prezzo}</th>
+                        ) : (
+                            <th>{prodottoInCarrello.prodotto.prezzo} (-{prodottoInCarrello.percSconto}%)</th>
+                        )
+                    }
+                    <th>{(prodottoInCarrello.prodotto.prezzo - ((prodottoInCarrello.prodotto.prezzo * prodottoInCarrello.percSconto) / 100)) * prodottoInCarrello.quantita}</th>
+                    <th>
+                        <Popup modal trigger={<button className="btn btn-warning">Ricette con questo prodotto</button>}>
+                            {close => <PopupContent close={close} titoloRicetta={prodottoInCarrello.prodotto.nome} />}
+                        </Popup>
+                    </th>
+                </tr>
+            ))}
+            </tbody>
+        )
+    }
+}
+/*const ProdottiInCarrello = ({ prodottiInCarrello }) => {
     return(
         <tbody>
             {prodottiInCarrello.map((prodottoInCarrello) => (
@@ -20,14 +55,14 @@ const ProdottiInCarrello = ({ prodottiInCarrello }) => {
                     }
                     <th>{(prodottoInCarrello.prodotto.prezzo - ((prodottoInCarrello.prodotto.prezzo * prodottoInCarrello.percSconto) / 100)) * prodottoInCarrello.quantita}</th>
                     <th>
-                        <Popup modal trigger={<button>Ricette con questo prodotto</button>}>
-                        {close => <PopupContent close={close} parametro={prodottoInCarrello.prodotto.nome} />}
+                        <Popup modal trigger={<button className="btn btn-warning">Ricette con questo prodotto</button>}>
+                        {close => <PopupContent close={close} titoloRicetta={prodottoInCarrello.prodotto.nome} ricette={ric[prodottoInCarrello.prodotto.nome]} />}
                         </Popup>
                     </th>
                 </tr>
             ))}
         </tbody>
     );
-};
+};*/
 
-export default ProdottiInCarrello
+export default ProdottiInCarrello;
